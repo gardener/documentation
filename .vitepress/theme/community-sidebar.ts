704dde80faa888246522a3e88d7835da237ac24a
klocke-io <niklas.klocke@sap.com>
2025-07-01 16:52:49 +0200

    feat: enhance content for empty index pages

--- .vitepress/theme/community-sidebar.ts
+++ .vitepress/theme/community-sidebar.ts
@@ -60,3 +60,49 @@ export function communitySidebar(): any {
 
   return generatedSidebar;
 }
+
+export function staticCommunitySidebar(): any {
+  return {
+    "/community/": {
+      "base": "/community/",
+      "items": [
+        {
+          "text": "Review Meetings",
+          "link": "review-meetings/index.md",
+          "items": [
+            {
+              "text": "Gardener Review Meetings 2025",
+              "link": "review-meetings/2025-reviews"
+            },
+            {
+              "text": "Gardener Review Meetings 2024",
+              "link": "review-meetings/2024-reviews"
+            },
+            {
+              "text": "Gardener Review Meetings 2023",
+              "link": "review-meetings/2023-reviews"
+            },
+            {
+              "text": "Gardener Review Meetings 2022",
+              "link": "review-meetings/2022-reviews"
+            },
+            {
+              "text": "Gardener Community Meetings 2022",
+              "link": "review-meetings/2022-community"
+            }
+          ],
+          "collapsed": true
+        },
+        {
+          "text": "Product Steering",
+          "link": "product-steering/index.md"
+        },
+        {
+          "text": "Technical Steering",
+          "link": "technical-steering/index.md"
+        }
+      ],
+      "text": "Community"
+    }
+  }
+}
