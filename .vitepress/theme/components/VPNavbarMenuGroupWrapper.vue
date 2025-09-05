e653f9d2edb277b94d60609843cb871136c67e23
Marc Vornetran <marc1404@users.noreply.github.com>
2025-09-05 10:01:44 +0200

    feat: Turn documentation in navbar into a link (#718)

---
+++ .vitepress/theme/components/VPNavbarMenuGroupWrapper.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import VPNavbarMenuGroup from 'vitepress/dist/client/theme-default/components/VPNavBarMenuGroup.vue'
+import VPLink from 'vitepress/dist/client/theme-default/components/VPLink.vue'
+
+const { text, link, activeMatch, items } = defineProps<{
+  text: string,
+  link: string,
+  activeMatch: string,
+  items: any[],
+}>()
+
+function handleClick() {
+  localStorage.setItem('lastClickedMenuItem', 'all')
+  window.dispatchEvent(new CustomEvent('menuItemClicked', {
+    detail: { value: 'all' }
+  }));
+}
+</script>
+
+<template>
+  <VPLink :href="link" @click.self="handleClick">
+    <VPNavbarMenuGroup :item="{ text, activeMatch, items }" />
+  </VPLink>
+</template>
